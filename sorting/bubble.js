e9749692c1a270407abeba25560ace76020f1edf
andrewkawetckiy <64364061+andrewkawetckiy@users.noreply.github.com>
2025-06-19 14:14:05 +0300

    Update bubble.js

--- sorting/bubble.js
+++ sorting/bubble.js
@@ -1,28 +1,25 @@
-// bubble.js
-
 async function bubbleSort(arr) {
+  const original = [...arr];
+  showOriginalArray(original);
+
   const n = arr.length;
   for (let i = 0; i < n - 1; i++) {
     for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        // Обмін елементів
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-
-        // Оновлюємо візуалізацію
         updateBar(j, arr[j]);
         updateBar(j + 1, arr[j + 1]);
-
-        // Затримка для анімації
-        await sleep(50);
+        await sleep(50); // регулює швидкість анімації
       }
     }
   }
+
+  showSortedArray(arr, "Bubble Sort");
 }
 
 function startBubble() {
-  console.log("startBubble викликана!");
   resetBars();
-  let arr = [...bars]; // копія масиву
+  let arr = [...bars];
   bubbleSort(arr);
 }
 
