a3d7e33a4186482316363c71b4f466d605d01a74
andrewkawetckiy <64364061+andrewkawetckiy@users.noreply.github.com>
2025-06-19 14:14:30 +0300

    Update quick.js

---
+++ sorting/quick.js
@@ -0,0 +1,39 @@
+async function quickSort(arr, low = 0, high = arr.length - 1) {
+  if (low < high) {
+    const pi = await partition(arr, low, high);
+
+    // Рекурсивно сортуємо ліву та праву частини
+    await quickSort(arr, low, pi - 1);
+    await quickSort(arr, pi + 1, high);
+  }
+
+  showSortedArray(arr, "Quick Sort");
+}
+
+async function partition(arr, low, high) {
+  const pivot = arr[high];
+  let i = low - 1;
+
+  for (let j = low; j <= high - 1; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      updateBar(i, arr[i]);
+      updateBar(j, arr[j]);
+      await sleep(50); // регулює швидкість анімації
+    }
+  }
+
+  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+  updateBar(i + 1, arr[i + 1]);
+  updateBar(high, arr[high]);
+  await sleep(50); // регулює швидкість анімації
+
+  return i + 1;
+}
+
+function startQuick() {
+  resetBars();
+  let arr = [...bars];
+  quickSort(arr);
+}
