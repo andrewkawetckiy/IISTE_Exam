afa6d9b399249433f30925e6008f41e37589c3b8
andrewkawetckiy <64364061+andrewkawetckiy@users.noreply.github.com>
2025-06-19 16:53:16 +0300

    Update quick.js

--- sorting/quick.js
+++ sorting/quick.js
@@ -1,31 +1,36 @@
-async function quickSort(arr, low = 0, high = arr.length - 1) {
-  if (low < high) {
-    const pi = await partition(arr, low, high);
-    await quickSort(arr, low, pi - 1);
-    await quickSort(arr, pi + 1, high);
-  }
+async function quickSort(arr) {
+  const copy = [...arr];
 
-  showSortedArray(arr, "Quick Sort");
-}
-
-async function partition(arr, low, high) {
-  const pivot = arr[high];
-  let i = low - 1;
-
-  for (let j = low; j <= high - 1; j++) {
-    if (arr[j] < pivot) {
-      i++;
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-      updateBar(i, arr[i]);
-      updateBar(j, arr[j]);
-      await sleep();
+  async function recursiveQuickSort(low, high) {
+    if (low < high) {
+      const pi = await partition(low, high);
+      await recursiveQuickSort(low, pi - 1);
+      await recursiveQuickSort(pi + 1, high);
     }
   }
 
-  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
-  updateBar(i + 1, arr[i + 1]);
-  updateBar(high, arr[high]);
-  await sleep();
+  async function partition(low, high) {
+    const pivot = copy[high];
+    let i = low - 1;
 
-  return i + 1;
+    for (let j = low; j <= high - 1; j++) {
+      if (copy[j] < pivot) {
+        i++;
+        [copy[i], copy[j]] = [copy[j], copy[i]];
+        updateBar(i, copy[i]);
+        updateBar(j, copy[j]);
+        await sleep();
+      }
+    }
+
+    [copy[i + 1], copy[high]] = [copy[high], copy[i + 1]];
+    updateBar(i + 1, copy[i + 1]);
+    updateBar(high, copy[high]);
+    await sleep();
+
+    return i + 1;
+  }
+
+  await recursiveQuickSort(0, copy.length - 1);
+  return copy;
 }
